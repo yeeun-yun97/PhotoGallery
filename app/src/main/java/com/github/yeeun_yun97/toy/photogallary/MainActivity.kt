6bc8dde4e9c15367910bb9fc68f8f7876d23b04b
yyn9704@gmail.com <yyn9704@gmail.com>
2022-09-07 15:26:45 +0900

    temp

--- app/src/main/java/com/github/yeeun_yun97/toy/photogallary/MainActivity.kt
+++ app/src/main/java/com/github/yeeun_yun97/toy/photogallary/MainActivity.kt
@@ -2,22 +2,29 @@ package com.github.yeeun_yun97.toy.photogallary
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.DragEvent
+import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
 import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat.startActivity
+import com.bumptech.glide.Glide
+import com.microsoft.device.dualscreen.draganddrop.DropContainer
+import com.microsoft.device.dualscreen.draganddrop.MimeType
 import com.skydoves.landscapist.glide.GlideImage
-import org.intellij.lang.annotations.JdkConstants
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,45 +35,92 @@ class MainActivity : ComponentActivity() {
                 verticalArrangement = Arrangement.Bottom,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                GlideImage(
-                    modifier = Modifier
-                        .size(300.dp)
-                        .pointerInput(Unit) {
-                                            detectDragGesturesAfterLongPress { change, dragAmount ->
-
-
-                                            }
-                        },
-                    imageModel = "https://thumbs.dreamstime.com/b/vector-empty-transparent-background-transparency-grid-seamless-pattern-171149540.jpg",
-
+                var imageUrl by remember {
+                    mutableStateOf(
+                        "https://thumbs.dreamstime.com/b/vector-empty-transparent-background-transparency-grid-seamless-pattern-171149540.jpg",
                     )
+                }
+//                var isDroppingImage by remember { mutableStateOf(false) }
+//                var isDroppingItem by remember { mutableStateOf(false) }
+//                DropContainer(
+//                    modifier = Modifier,
+//                    onDrag = { inBounds, isDragging ->
+//                        if (!inBounds || !isDragging) {
+//                            isDroppingImage = false
+//                        }
+//                        isDroppingItem = isDragging
+//                    }
+//                ) { dragData ->
+//                    dragData?.also {
+//                        if (dragData.type == MimeType.IMAGE_JPEG) {
+//                            isDroppingImage = isDroppingItem
+//                            if (!isDroppingItem) {
+//                                imageUrl = dragData.data as String
+//                            }
+//                        }
+//                    }
+//                    GlideImage(
+//                        modifier = Modifier
+//                            .size(300.dp)
+//                            .pointerInput(Unit) {
+//                                detectDragGesturesAfterLongPress { change, dragAmount ->
+//                                }
+//                            },
+//                        imageModel = imageUrl
+//                    )
+
+                    AndroidView(
+                        modifier = Modifier.size(300.dp),
+                        factory = { it ->
+                            val imageView = ImageView(it)
+                            Glide.with(it).load(imageUrl).centerCrop().into(imageView)
+                            imageView.setOnDragListener { view, dragEvent ->
+                                if (dragEvent.action == DragEvent.ACTION_DROP) {
+                                    val imgView = view as ImageView
+                                    val item = dragEvent.clipData.getItemAt(0)
+                                    val dragData: String = item.uri.toString()
+                                    imageUrl = dragData
+                                    Glide.with(it).load(dragData).centerCrop().into(imgView)
+                                    view.invalidate()
+                                }
+                                return@setOnDragListener true
+                            }
+                            imageView
+                        })
+
+
+                }
                 Spacer(modifier = Modifier.height(40.dp))
                 Text("hello, world!")
+
                 Spacer(modifier = Modifier.height(15.dp))
-            }
-        }
-    }
+
+                Button(onClick = { openInMulti() }){
+                    Text("click me to open multi window")
+                }
 
 
+                Spacer(modifier = Modifier.height(15.dp))
 
+//            }
+            //DragAndDropApp()
 
-
-    override fun onResume() {
-        super.onResume()
-        if (!this.isInMultiWindowMode) {
-            openInMulti()
         }
     }
 
     private fun openInMulti() {
-        val intent = Intent(this@MainActivity, ListPhotoActivity::class.java)
-        intent.addFlags(
-            Intent.FLAG_ACTIVITY_LAUNCH_ADJACENT
-                    or
-                    Intent.FLAG_ACTIVITY_MULTIPLE_TASK
-                    or
-                    Intent.FLAG_ACTIVITY_NEW_TASK
-        )
-        startActivity(intent)
+        if (!this.isInMultiWindowMode) {
+            val intent = Intent(this@MainActivity, ListPhotoActivity::class.java)
+            intent.addFlags(
+                Intent.FLAG_ACTIVITY_LAUNCH_ADJACENT
+                        or
+                        Intent.FLAG_ACTIVITY_MULTIPLE_TASK
+                        or
+                        Intent.FLAG_ACTIVITY_NEW_TASK
+            )
+            startActivity(intent)
+        }
     }
+
+
 }
---
+++ app/src/main/java/com/github/yeeun_yun97/toy/photogallary/imp/DragPane.kt
@@ -0,0 +1,104 @@
+/*
+ *
+ *  * Copyright (c) Microsoft Corporation. All rights reserved.
+ *  * Licensed under the MIT License.
+ *
+ */
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.MaterialTheme.typography
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.github.yeeun_yun97.toy.photogallary.R
+import com.microsoft.device.display.samples.draganddrop.ui.view.TopBarWithTitle
+import com.microsoft.device.dualscreen.draganddrop.DragData
+import com.microsoft.device.dualscreen.draganddrop.DragTarget
+import com.microsoft.device.dualscreen.draganddrop.MimeType
+
+@Composable
+fun DragPaneWithTopBar() {
+    Scaffold(
+        topBar = { TopBarWithTitle() }
+    ) {
+        DragPane(paddingValues = it)
+    }
+}
+
+@Composable
+fun DragPane(modifier: Modifier = Modifier, paddingValues: PaddingValues = PaddingValues()) {
+    Row(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(paddingValues)
+            .background(Color(0xFFEDEDED))
+            .testTag(stringResource(R.string.drag_pane))
+    ) {
+        DragImageBox()
+        Spacer(modifier = Modifier.width(30.dp))
+        DragTextBox()
+    }
+}
+
+@Composable
+fun RowScope.DragImageBox() {
+    val dragImage = painterResource(id = R.drawable.drag_and_drop_image)
+    val dragData = DragData(type = MimeType.IMAGE_JPEG, data = dragImage)
+
+    Box(
+        modifier = Modifier
+            .weight(1f)
+            .fillMaxHeight()
+            .padding(20.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        DragTarget(dragData = dragData) {
+            Image(
+                painter = dragImage,
+                contentDescription = stringResource(R.string.image_contentDescription),
+                // to get a fix frame of the shadow
+                modifier = Modifier.size(width = 300.dp, height = 120.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun RowScope.DragTextBox() {
+    val dragText = stringResource(R.string.drag_and_drop_plain_text)
+    val dragData = DragData(type = MimeType.TEXT_PLAIN, data = dragText)
+
+    Box(
+        modifier = Modifier
+            .weight(1f)
+            .fillMaxHeight()
+            .padding(20.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        DragTarget(dragData = dragData) {
+            Text(
+                text = dragText,
+                style = typography.body1,
+                // to get a fix frame of the shadow
+                modifier = Modifier.size(width = 250.dp, height = 300.dp)
+            )
+        }
+    }
+}
